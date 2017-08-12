0a1d610fa6386c748a29fcfefe554d67017739e2
Edukni <eduk_1012@hotmail.com>
2017-08-12 15:02:00 -0500

    first commit

---
+++ e-search.js
@@ -0,0 +1,60 @@
+(function($){
+    $.fn.extend({
+        search: function(callback,timeout){
+            timeout = timeout || 1e3; // 1 second default timeout
+            var timeoutReference,
+                search = function(el){
+                    if (!timeoutReference) return;
+                    timeoutReference = null;
+                    callback.call(el);
+                };
+            return this.each(function(i,el){
+                var $el = $(el);
+                // Chrome Fix (Use keyup over keypress to detect backspace)
+                // thank you @palerdot
+                $el.is(':input') && $el.on('keyup keypress paste',function(e){
+                    // This catches the backspace button in chrome, but also prevents
+                    // the event from triggering too preemptively. Without this line,
+                    // using tab/shift+tab will make the focused element fire the callback.
+                    if (e.type=='keyup' && e.keyCode!=8) return;
+                    
+                    // Check if timeout has been set. If it has, "reset" the clock and
+                    // start over again.
+                    if (timeoutReference) clearTimeout(timeoutReference);
+                    timeoutReference = setTimeout(function(){
+
+
+			     		var valor = el.value.toLowerCase();
+			     		if(valor){
+
+			     		$(".containerItems").children().fadeOut('fast');
+			     		$(".containerItems [data-search*="+valor+"]").fadeIn('fast');
+
+			     		}else{
+
+                          $(".containerItems").children().fadeIn('fast');
+        
+
+                        }
+
+                        
+
+                        search(el);
+
+                    }, timeout);
+                }).on('blur',function(){
+     	   				//when leaving the input
+
+                   		search(el);
+                });
+            });
+
+
+
+        }
+    });
+})(jQuery);
+
+
+
+
